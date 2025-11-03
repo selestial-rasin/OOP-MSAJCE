cdc0282e0ef1f508631b244562427c4035d72434
Selestial.Rasin <rasinsallai@gmail.com>
2025-11-03 12:08:30 +0530

    Rename Addition class to Add

---
+++ HW/Add.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Add {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second integer: ");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        input.close();
+    }
+}
