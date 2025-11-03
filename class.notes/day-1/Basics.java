80af834bfb98370f85a3a5ca1883059c5881a735
Selestial.Rasin <rasinsallai@gmail.com>
2025-11-03 14:13:28 +0530

    Update Basics.java

--- class.notes/day-1/Basics.java
+++ class.notes/day-1/Basics.java
@@ -1,9 +1,14 @@
 public class Basics {
-  public static void main(string args[]){
-    System.out.println("This is printed by print statement")
-      System.out.println("Datatypes")
-      int num=30;
-    System.out.printf("this is integer %d,num);
-    
+  public static void main(String args[]){
+    System.out.println("This is printed by print statement");
+    System.out.println("Datatypes");
+    int num=30;
+    System.out.printf("This is Integer %d\n",num);
+    short numshort=234;
+    System.out.printf("This is Short Integer %d\n",numshort);
+    long numlong=2389247;
+    System.out.printf("This is Long Integer %d\n",numlong);
+    float numdecimal=10.7f;
+    System.out.printf("This is float %f\n" ,numdecimal);
   }
 }
