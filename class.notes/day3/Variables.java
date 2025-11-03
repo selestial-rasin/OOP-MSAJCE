2cf7f52c0cb8e54282afed865adf444587a5d1b3
Selestial.Rasin <rasinsallai@gmail.com>
2025-11-03 14:37:50 +0530

    Add Variables class with instance and static variables

---
+++ class.notes/day3/Variables.java
@@ -0,0 +1,20 @@
+class Variables {
+  int a = 10;           
+  static int b = 20;      
+
+  void show() {
+    int c = 30;         
+    System.out.println("Local variable: " + c);
+    System.out.println("Instance variable: " + a);
+    System.out.println("Static variable: " + b);
+  }
+}
+
+public class Basics {
+  public static void main(String[] args) {
+    Example obj1 = new Example(); 
+    obj1.show();
+
+    System.out.println("Accessing static variable without object: " + Example.b);
+  }
+}
