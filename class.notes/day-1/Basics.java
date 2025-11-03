1c950b7b1267ab8082f0a33b0324ec3b32e2d853
Selestial.Rasin <rasinsallai@gmail.com>
2025-11-03 12:50:44 +0530

    Create Basics.java with main method and print statements
    Fix syntax errors in Basics.java and add print statements.

---
+++ class.notes/day-1/Basics.java
@@ -0,0 +1,8 @@
+public class Basics {
+  public static void main(string args[]){
+    System.out.println("This is printed by print statement")
+      System.out.println("Datatypes")
+      int num=30;
+    System.out.println("this is integer %d,num);
+  }
+}
