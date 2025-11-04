e4ba725729742d366eaaea4302b8e7931a5fc013
Selestial.Rasin <rasinsallai@gmail.com>
2025-11-04 11:51:28 +0530

    Implement method overloading in Calculator class

---
+++ class.notes/day-4/polymorphism/Overloading.java
@@ -0,0 +1,27 @@
+class Calculator {
+
+  
+  void add(int a, int b) {
+    System.out.println("Sum of two numbers: " + (a + b));
+  }
+
+  
+  void add(int a, int b, int c) {
+    System.out.println("Sum of three numbers: " + (a + b + c));
+  }
+
+  
+  void add(double a, double b) {
+    System.out.println("Sum of doubles: " + (a + b));
+  }
+}
+
+public class Basics {
+  public static void main(String[] args) {
+    Calculator calc = new Calculator();
+
+    calc.add(10, 20);          
+    calc.add(10, 20, 30);      
+    calc.add(5.5, 4.5);        
+  }
+}
