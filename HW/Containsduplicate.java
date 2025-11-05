82288a6e0207667bbba576d28e007e1a69c0e048
Selestial.Rasin <rasinsallai@gmail.com>
2025-11-05 14:21:15 +0530

    Create Containsduplicate.java

---
+++ HW/Containsduplicate.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums));
+    }
+}
